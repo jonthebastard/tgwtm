4a5b7acf5fc3cc5261231a7a6e2a62cd5e01ea09
jonathan.pirro <jonathan.pirro@wbgames.com>
2018-03-09 00:05:38 -0800

    new image randomization

---
+++ static/js/randimage.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    var parent = $("#bg");
+    var divs = parent.children();
+    while (divs.length) {
+        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+    }
+});
---
+++ static/js/slickbg.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.slick-bg').slick({
+    arrows: false,
+    accessibility: false,
+    lazyLoad: 'ondemand',
+    infinite: true,
+    fade: true,
+    cssEase: 'linear',
+    speed: 1500,
+    autoplay: true,
+    autoplaySpeed: 5000
+  });
+});
